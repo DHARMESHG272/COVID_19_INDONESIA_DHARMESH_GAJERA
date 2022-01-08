734c5bf8a1b67f60215217c5c34635d2a0abf5f5
gdharmesh5@gmail.com <gdharmesh5@gmail.com>
2022-01-08 10:30:18 +0000

    intermidiate commit

---
+++ models/marts/intermediate/covid19_indonesia.sql
@@ -0,0 +1,26 @@
+with covid19_indonesia as (
+
+    select  Date,
+            location_iso_code as location_code,
+            New_Cases as Cases,
+            New_Deaths as Deaths,
+            New_Recovered as Recovered,
+            New_Active_Cases as Active_Cases,
+            Total_Cases,
+            Total_Deaths,
+            Total_Recovered,
+            Total_Active_Cases,
+            New_Cases_per_Million as Cases_per_Million,
+            Total_Cases_per_Million,
+            New_Deaths_per_Million as Deaths_per_Million,
+            Total_Deaths_per_Million,
+            Case_Fatality_Rate,
+            Case_Recovered_Rate,
+            Growth_Factor_of_New_Cases as Growth_Factor_of_Cases,
+            Growth_Factor_of_New_Deaths as Growth_Factor_of_Deaths
+    from fivetran_interview_db.google_sheets.covid_19_indonesia_dharmesh_gajera
+    order by cast(date as date) desc
+
+)
+
+select * from covid19_indonesia
--- models/marts/intermediate/locations.sql
+++ models/marts/intermediate/locations.sql
@@ -1,14 +1,26 @@
 with locations as (
 
-    select Location_ISO_Code as Location_code,
-           Location,
-           Location_Level,
-           Province,
-           Country,
-           Continent,
-           time_zone
+    select location_iso_code as location_code,
+           location,
+           location_level,
+           province,
+           country,
+           continent,
+           time_zone,
+           total_regencies,
+           total_cities,
+           total_districts,
+           total_urban_villages,
+           total_rural_villages,
+           area_km_2_ as area_km2,
+           population,
+           population_density,
+           longitude,
+           latitude
     from fivetran_interview_db.google_sheets.covid_19_indonesia_dharmesh_gajera
-    group by Location_ISO_Code, Location, Location_Level, Province, Country, Continent, time_zone
+    group by location_iso_code, location, location_level, province, country, continent, time_zone,
+             total_regencies, total_cities, total_districts, total_urban_villages, total_rural_villages,
+             area_km_2_, population, population_density, longitude, latitude
 )
 
 select * from locations
